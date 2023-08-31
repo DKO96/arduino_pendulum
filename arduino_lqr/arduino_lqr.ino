d789ff84ede9a3f59f384ab4baf0347e33ee381e
DKO96 <danielwjkhoo@gmail.com>
2023-08-31 16:37:57 -0400

    kalman filter

--- arduino_lqr/arduino_lqr.ino
+++ arduino_lqr/arduino_lqr.ino
@@ -1,72 +1,72 @@
-#include <MPU6050.h>
+#include <Arduino.h>
 #include <Wire.h>
+#include <MPU6050.h>
 #include <Servo.h>
 
 #define ACCELE_RANGE 4
 #define GYROSC_RANGE 500
+#define BIT_RANGE 65536
+#define TIMESTEP 0.005
 
-Servo ESC;
-
-int STOP_CCW = 88; 
-int STOP_CW = 85;
-int CCW = 83;
-int CW = 90;
-
+// SENSOR VARIABLES 
 const int MPU_addr = 0x68; // I2C address of the MPU-6050
 float AcX, AcY, AcZ, Tmp, GyX, GyY, GyZ;
-double g = 9.81;
-double yawFiltered = 0;
-const double alpha = 0.05;
+float yaw = 0;
+float KalmanYaw = 0, KalmanUncertaintyYaw = 4;
+float KalmanOutput[] = {0,0};
 
-double yawOffset = 0.0;
-bool isCalibrated = false;
+// MOTOR VARIABLES
+Servo ESC;
+const int CW = 90;
+const int STOP_CW = 85;
+const int CCW = 83;
+const int STOP_CCW = 88;
 
 
-
-
-
-
-
+void MPU_signal();
+void KalmanFilter(float &KalmanState, float &KalmanUncertainty,
+                  float KalmanInput, float KalmanMeasurement);
 
 void setup() {
-  Serial.begin(9600);
-  ESC.attach(9, 1000, 2000);
-
   Wire.begin();
   Wire.beginTransmission(MPU_addr);
   Wire.write(0x6B);  // PWR_MGMT_1 register
   Wire.write(0);     // set to zero (wakes up the MPU-6050)
   Wire.endTransmission(true);
-
-  calibrateYaw();
+  ESC.attach(9, 1000, 2000);  
+  Serial.begin(115200);
 }
-
-
-void calibrateYaw() {
-  int calibrationSamples = 100;
-  double yawSum = 0;
-
-    for (int i = 0; i < calibrationSamples; i++) {
-      Wire.beginTransmission(MPU_addr);
-      Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
-      Wire.endTransmission(false);
-      Wire.requestFrom(MPU_addr, 14, true); // request a total of 14 registers
-      AcX = Wire.read() << 8 | Wire.read(); // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
-      AcY = Wire.read() << 8 | Wire.read(); // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
-
-      yawSum += atan2(AcY, AcX) * (180.0 / PI);
-
-      delay(10);  // Short delay between readings
-    }
-
-    yawOffset = yawSum / calibrationSamples;
-    isCalibrated = true;
-    Serial.print("Calibration Offset: ");
-    Serial.println(yawOffset);
-}
-
 
 void loop() {
+  MPU_signal();
+  KalmanFilter(KalmanYaw, KalmanUncertaintyYaw, GyZ, yaw);
+  Serial.println(KalmanYaw);
+  yaw = KalmanYaw;
+
+  //Motor test
+  if (yaw > 5.0) {
+    ESC.write(CW);
+  } 
+  else if (yaw < 5.0 && yaw > 0.0) {
+    ESC.write(STOP_CW);
+  }
+
+  // For CCW direction of pendulum
+  if (yaw < -5.0) {
+    ESC.write(CCW);
+  }
+  else if (yaw > -5.0 && yaw < 0.0) {
+    ESC.write(STOP_CCW);
+  }
+
+
+  delay(TIMESTEP*1000);
+}
+
+///////////////////////////////////////////////////////////
+///// FUNCTIONS 
+//////////////////////////////////////////////////////////
+void MPU_signal(){
   Wire.beginTransmission(MPU_addr);
   Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
   Wire.endTransmission(false);
@@ -79,37 +79,22 @@ void loop() {
   GyY = Wire.read() << 8 | Wire.read(); // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
   GyZ = Wire.read() << 8 | Wire.read(); // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
 
-  float AcX_g = AcX / 65536 * ACCELE_RANGE;
-  float AcY_g = AcY / 65536 * ACCELE_RANGE;
-  // double yaw = atan2(AcY_g, AcX_g) * (180.0 / PI);
-  double yaw = atan2(AcY_g, AcX_g) * (180.0 / PI) - yawOffset;
+  AcX = AcX/BIT_RANGE * ACCELE_RANGE;
+  AcY = AcY/BIT_RANGE * ACCELE_RANGE;
+  AcZ = AcZ/BIT_RANGE * ACCELE_RANGE;
+  GyX = GyX/BIT_RANGE * ACCELE_RANGE;
+  GyY = GyY/BIT_RANGE * ACCELE_RANGE;
+  GyZ = GyZ/BIT_RANGE * ACCELE_RANGE;
 
-  yawFiltered = alpha * yaw + (1.0 - alpha) * yawFiltered;
-  // yawFiltered = yawFiltered - 0.2;
+  yaw = atan2(AcY, AcX) * (180.0/PI); 
+}
 
-  // Serial.print("Filtered Yaw (degs): ");
-  Serial.print(-60);
-  Serial.print(" ");
-  Serial.print(60);
-  Serial.print(" ");
-  Serial.println(yawFiltered);
+void KalmanFilter(float &KalmanState, float &KalmanUncertainty, 
+                  float KalmanInput, float KalmanMeasurement) {
+  KalmanState = KalmanState + TIMESTEP*KalmanInput;
+  KalmanUncertainty = KalmanUncertainty + TIMESTEP*TIMESTEP*16;
 
-  // For CW direction of pendulum
-  if (yawFiltered > 5.0) {
-    ESC.write(CW);
-  } 
-  else if (yawFiltered < 5.0 && yawFiltered > 0.0) {
-    ESC.write(STOP_CW);
-  }
-
-  // For CCW direction of pendulum
-  if (yawFiltered < -5.0) {
-    ESC.write(CCW);
-  }
-  else if (yawFiltered > -5.0 && yawFiltered < 0.0) {
-    ESC.write(STOP_CCW);
-  }
-
-  delay(1);
-
+  float KalmanGain = KalmanUncertainty * 1/(1*KalmanUncertainty + 9);
+  KalmanState = KalmanState + KalmanGain*(KalmanMeasurement - KalmanState);
+  KalmanUncertainty = (1 - KalmanGain) * KalmanUncertainty;
 }
